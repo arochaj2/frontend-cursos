dc964d35463de3a2c1a7a704921e97f69478f369
arochaj2 <arochaj2@gmail.com>
2023-04-04 16:42:51 +0200

    agregando los services

--- src/app/components/alumnos/alumnos.component.ts
+++ src/app/components/alumnos/alumnos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/models/alumno';
+import { AlumnoService } from 'src/app/services/alumno.service';
 
 @Component({
   selector: 'app-alumnos',
@@ -8,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class AlumnosComponent implements OnInit {
 
 titulo='Listado de Alumnos';
+alumnos: Alumno[];
 
-  constructor() { }
+  constructor(private service: AlumnoService) { }
 
   ngOnInit(): void {
+
+    this.service.listar().subscribe( alumnos => this.alumnos=alumnos );
+
+
   }
 
 }
